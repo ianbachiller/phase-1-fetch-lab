58ac47512c0229866fff7f3c2e7a748378512e20
Ian Bachiller <ianbachise@gmail.com>
2024-02-12 15:51:32 -0500

    updated to add 5th book and total number of pages answers (still solving 1031st char)

--- index.js
+++ index.js
@@ -8,14 +8,38 @@ function fetchBooks() {
 
 function renderBooks(books) {
   const main = document.querySelector('main');
+
+  const h1 = document.createElement('h1')
+  h1.innerHTML = `The fifth book is: ${books[4].name} and it has ${books[4].numberOfPages} pages. `
+  main.appendChild(h1)
+
+  const char = document.createElement('h1')
+  char.innerHTML = `The 1031st character is: `
+  main.appendChild(char)
+
+  const page = document.createElement('h1')
+  page.innerHTML = `The total number of all pages is: ${(totalNumberOfPages(books))} `
+  main.appendChild(page)
+
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name
+    h2.innerHTML = `The book titled ${book.name} has ${book.numberOfPages} pages`
+    console.log(book)
     main.appendChild(h2);
+
   });
+}
+
+function totalNumberOfPages(books){
+  const totalPages = books.reduce((acc, book) => acc + book.numberOfPages, 0)
+  return totalPages
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
 
+// //find: 
+// The 5th book in the series
+// The 1031st character in the series
+// The total number of pages of all the books
